94571c6b8c9344ecf6db8f918543125a94028477
Wojciech Pituła <wojciech@swissborg.com>
2023-07-20 08:42:57 +0200

    WIP sphere.it

---
+++ 2023-09-sphere-it/resources/generate.sh
@@ -0,0 +1,3 @@
+#mmdc -i comm-model.mermaid -o comm-model.mermaid.png -b transparent -w 1600 -H 800 -s 4
+mmdc -i comm-model-2.mermaid -o comm-model-2.mermaid.png -b transparent -w 1600 -H 800 -s 4
+mmdc -i comm-model-3.mermaid -o comm-model-3.mermaid.png -b transparent -w 1600 -H 800 -s 4
