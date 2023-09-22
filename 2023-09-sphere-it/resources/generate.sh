1fffbc95b9a9fcb5e6cfdce4c828d2e7d5be31c9
Wojciech Pituła <wojciech@swissborg.com>
2023-09-22 21:45:15 +0200

    fixes till 22

--- 2023-09-sphere-it/resources/generate.sh
+++ 2023-09-sphere-it/resources/generate.sh
@@ -1,3 +1,4 @@
 #mmdc -i comm-model.mermaid -o comm-model.mermaid.png -b transparent -w 1600 -H 800 -s 4
-mmdc -i comm-model-2.mermaid -o comm-model-2.mermaid.png -b transparent -w 1600 -H 800 -s 4
-mmdc -i comm-model-3.mermaid -o comm-model-3.mermaid.png -b transparent -w 1600 -H 800 -s 4
+#mmdc -i comm-model-2.mermaid -o comm-model-2.mermaid.png -b transparent -w 1600 -H 800 -s 4
+#mmdc -i comm-model-3.mermaid -o comm-model-3.mermaid.png -b transparent -w 1600 -H 800 -s 4
+mmdc -i breadcrumbs.mermaid -o breadcrumbs.mermaid.png -b transparent -w 800 -H 400 -s 4
